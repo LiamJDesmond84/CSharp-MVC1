c050b358398f34f7413e33991352d702c8e5f342
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-17 17:38:41 -0500

    Adding 1st class

--- Models/Friend.cs
+++ Models/Friend.cs
@@ -2,5 +2,9 @@
 {
     public class Friend
     {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
     }
 }
