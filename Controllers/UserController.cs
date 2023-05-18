8f92a5dc675a4e9254447e29ffcc91f4dbd8c611
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-18 17:37:52 -0500

    Implementing View

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,22 @@
-﻿namespace CSharp_MVC1.Controllers
+﻿using CSharp_MVC1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharp_MVC1.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
+
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+
+            User user = new User();
+
+            user.Name = "Karl";
+            user.Email = "kpilk@gmail.com";
+
+            return View();
+        }
     }
 }
