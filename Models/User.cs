78e96723ccd6713a7b2860a706e065fc0700e642
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-23 17:06:30 -0500

    Updating model

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,8 @@
 {
     public class User
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
