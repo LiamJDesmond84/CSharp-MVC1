590829f50c57d574c02642c5109ff8299d84dc97
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-24 18:46:50 -0500

    Testing View forloop for Model List

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,9 +7,12 @@ namespace CSharp_MVC1.Controllers
     {
 
 
+
         [HttpGet("/UserIndex")]
         public IActionResult Index()
         {
+
+            List<User> users = new List<User>();
 
             User user = new User();
 
@@ -17,8 +20,17 @@ namespace CSharp_MVC1.Controllers
             user.LastName = "Pilkington";
             user.Email = "kpilk@gmail.com";
 
+            User user2 = new User();
+
+            user.FirstName = "Ricky";
+            user.LastName = "Gervais";
+            user.Email = "rickyg@gmail.com";
+
             // Using @ViewBag in cshtml
             ViewBag.User = user;
+
+            users.Add(user);
+            users.Add(user2);
 
             // Using @Model in cshtml(user argument)
                 // "Index" if you wanted to name a specific View file
